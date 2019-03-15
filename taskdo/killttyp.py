75b2e8add9b309ea19320f952b00e558f78c6105
LettyCai <sei_clj@126.com>
2019-03-15 16:07:37 +0000

    modify the issue that killttyp script passing incorrect password

--- taskdo/killttyp.py
+++ taskdo/killttyp.py
@@ -14,28 +14,23 @@ from ansible.inventory.host import Host,Group
 from collections import namedtuple
 #from ansible_call import AnsibleRunner,MyInventory
 
-class MyInventory():
-    def __init__(self):
-        # self.resource = resource
-        self.loader = DataLoader()
-        self.inventory = InventoryManager(loader=self.loader, sources=['/root/OpsAuto/conf/hostslist'])
-        self.variable_manager = VariableManager(loader=self.loader, inventory=self.inventory)
+
+
 
 class ModelResultsCollector(CallbackBase):
-    # 改写返回方法
+    """
+    重写callbackBase类的部分方法
+    """
     def __init__(self, *args, **kwargs):
         super(ModelResultsCollector, self).__init__(*args, **kwargs)
         self.host_ok = {}
         self.host_unreachable = {}
         self.host_failed = {}
-
     def v2_runner_on_unreachable(self, result):
         self.host_unreachable[result._host.get_name()] = result
-
-    def v2_runner_on_ok(self, result, *args, **kwargs):
+    def v2_runner_on_ok(self, result):
         self.host_ok[result._host.get_name()] = result
-
-    def v2_runner_on_failed(self, result, *args, **kwargs):
+    def v2_runner_on_failed(self, result):
         self.host_failed[result._host.get_name()] = result
 
 
@@ -45,9 +40,6 @@ class AnsibleRunner(object):
         """
 
     def __init__(self, logId=None, *args, **kwargs):
-        self.inventory = None
-        # self.variable_manager = None
-        self.loader = None
         self.options = None
         self.passwords = None
         self.callback = None
@@ -64,8 +56,9 @@ class AnsibleRunner(object):
                                          'scp_extra_args', 'become', 'become_method', 'become_user', 'ask_value_pass',
                                          'verbosity',
                                          'check', 'listhosts', 'listtasks', 'listtags', 'syntax', 'diff'])
+        #self.loader = DataLoader()
 
-        self.loader = DataLoader()
+
         self.options = Options(connection='smart', module_path=None, forks=100, timeout=10,
                                remote_user='root', ask_pass=False, private_key_file=None, ssh_common_args=None,
                                ssh_extra_args=None,
@@ -74,11 +67,9 @@ class AnsibleRunner(object):
                                listtasks=False, listtags=False, syntax=False, diff=True)
 
         self.passwords = dict(sshpass=None, becomepass=None)
-        myinvent = MyInventory()
-        self.inventory = myinvent.inventory
-        self.variable_manager = myinvent.variable_manager
 
-    def run_modle(self, host_list, variable_manager, module_name, module_args):
+
+    def run_modle(self,inventory,variable_manager,loader,host_list,module_name,module_args):
         """
             run module from andible ad-hoc.
             module_name: ansible module_name
@@ -86,11 +77,11 @@ class AnsibleRunner(object):
         """
 
         play_source = dict(name="Ansible Play",
-                           hosts=['192.168.205.10'],
+                           hosts=host_list,
                            gather_facts='no',
-                           tasks=[dict(action=dict(module='shell', args='touch /root/aaaaaaaaaa.txt'))])
+                           tasks=[dict(action=dict(module=module_name, args=module_args))])
 
-        play = Play().load(play_source, variable_manager=variable_manager, loader=self.loader)
+        play = Play().load(play_source,variable_manager=variable_manager,loader=loader)
         # tqm = None
         # if self.redisKey:self.callback = ModelResultsCollectorToSave(self.redisKey,self.logId)
         # else:self.callback = ModelResultsCollector()
@@ -98,9 +89,9 @@ class AnsibleRunner(object):
         import traceback
         # try:
         tqm = TaskQueueManager(
-            inventory=self.inventory,
+            inventory=inventory,
             variable_manager=variable_manager,
-            loader=self.loader,
+            loader=loader,
             options=self.options,
             passwords=self.passwords,
             stdout_callback=callback,
@@ -108,12 +99,13 @@ class AnsibleRunner(object):
         # constants.HOST_KEY_CHECKING = False  # 关闭第一次使用ansible连接客户端时输入命令
         result = tqm.run(play)
 
-
         result_raw = {'success': {}, 'failed': {}, 'unreachable': {}}
         for host, result in callback.host_ok.items():
             result_raw['success'][host] = result._result
         for host, result in callback.host_failed.items():
             result_raw['failed'][host] = result._result
+        for host, result in callback.host_unreachable.items():
+            result_raw['unreachable'][host] = result._result
 
         print(result_raw)
 
@@ -125,22 +117,22 @@ class AnsibleRunner(object):
     #      if tqm is not None:
     #   tqm.cleanup()
 
-
-
-
-
-
 loader = DataLoader()
 inventory = InventoryManager(loader=loader,sources=['/root/OpsAuto/conf/hostslist'])
 host = inventory.get_host(hostname='192.168.205.10')
 variablemanager = VariableManager(loader=loader, inventory=inventory)
-variablemanager.set_host_variable(host=host, varname='ansible_ssh_password', value='123456')
+variablemanager.set_host_variable(host=host,varname='ansible_ssh_pass',value='123456')
 model = 'shell'
-args = 'touch /root/abcd.txt'
+args = 'touch /root/abcdppppppppppp.txt'
+
+hs = inventory.get_hosts()
+print(hs)
+hosts = []
+for host in hs:
+    hosts.append(host)
+print(hosts)
+
+#hosts = ['192.168.205.10','192.168.205.11']
 
 ans = AnsibleRunner()
-hosts =['192.168.56.102']
-ans.run_modle(hosts,variablemanager,'shell','touch /root/aaaaaaaaaa.txt')
-
-
-
+ans.run_modle(inventory=inventory,host_list=hosts,loader=loader,variable_manager=variablemanager,module_name=model,module_args=args)
