07ddf80f955f41aeb31529905ad2ce77d0192d92
LettyCai <sei_clj@126.com>
2019-05-20 14:53:50 +0000

    20190502commit

--- taskdo/models.py
+++ taskdo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class LogModel(models.Model):
+    user = models.CharField(max_length=20,verbose_name=u"用户名",null=True)
+    hosts = models.CharField(max_length=20,verbose_name=u"主机",null=True)
+    result = models.CharField(max_length=20,verbose_name=u"执行结果",null=True)
+    logs = models.CharField(max_length=500,verbose_name=u"操作日志",null=True)
+
+    class Meta:
+        verbose_name = u'操作日志'
+        verbose_name_plural = verbose_name
+        db_table = 'logs'
