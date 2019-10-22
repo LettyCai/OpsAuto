b03030f116d1f26fb8b297d1b805b1f7bfdeb463
LettyCai <sei_clj@126.com>
2019-10-22 08:52:51 +0000

    20191022

--- OpsAuto/settings.py
+++ OpsAuto/settings.py
@@ -166,9 +166,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT='/root/OpsAuto/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#)
 
 LOGIN_URL = '/login/'
